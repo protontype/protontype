7f5008cc8ea32a64b294d311467a0da4babb05dd
Humberto Machado <betolinck@gmail.com>
2016-08-03 23:09:18 -0300

    Unnecessary code

--- src/routes/ExpressRouter.ts
+++ src/routes/ExpressRouter.ts
@@ -1,5 +1,4 @@
 import { ExpressApplication } from './../libs/ExpressApplication';
-import {SequelizeModel} from "../models/SequelizeModel";
 
 /**
  * Express routes configurations
@@ -12,13 +11,12 @@ export abstract class ExpressRouter {
     constructor(expressApplication: ExpressApplication) {
         this.express = expressApplication.getExpress();
         this.model = expressApplication.getSequelizeDB().getModel(this.modelName);
-        this.start();
     }
 
     protected sendErrorMessage(res: any, error: any): void {
         res.status(412).json({msg: error.message})
     }
-    public abstract start(): void;
+
     public abstract start(): void;
 }
 
