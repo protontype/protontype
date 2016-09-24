e14416d764288a86d29f4a02d1118b433305f091
Humberto Machado <betolinck@gmail.com>
2016-09-23 23:10:17 -0300

    Update to typescript 2.0 and npm types definitions

--- src/libs/ExpressApplication.ts
+++ src/libs/ExpressApplication.ts
@@ -31,7 +31,7 @@ export class ExpressApplication {
     public bootstrap(): Express.Application {
         this.configMiddlewares();
         let port: number = this.express.get("port");
-        this.sequelizeDB.getInstance().sync().done(() => {
+        this.sequelizeDB.getInstance().sync().then(() => {
             this.configureRoutes();
             this.express.listen(port, () => console.log(`Application listen on port ${port}`));
         });
