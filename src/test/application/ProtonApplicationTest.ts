cef2652291294d7cbf891fc18736d3fd1d642d0f
Humberto Machado <betolinck@gmail.com>
2017-08-02 15:20:44 -0300

    Tests improvements

--- src/test/application/ProtonApplicationTest.ts
+++ src/test/application/ProtonApplicationTest.ts
@@ -1,3 +1,5 @@
+import { JsonContentMiddleware } from './../../lib/middlewares/JsonContentMiddleware';
+import { Mock, Mock2 } from './../utils/ModelMock';
 import { ProtonApplication } from '../../lib';
 import { GlobalConfig } from '../../lib';
 import {
@@ -22,6 +24,7 @@ class ProtonApplicationTest {
         this.config = JsonLoader.loadFile<GlobalConfig>("./src/test/utils/config.json");
         this.app = new ProtonApplication(this.config)
             .addMiddleware(new GlobalMiddlewareMock())
+            .addMiddleware(new JsonContentMiddleware())
             .addRouter(new RouterMock());
         done();
     }
@@ -80,6 +83,9 @@ class ProtonApplicationTest {
                 assert.equal(res.body.globalMidMsg, GLOBAL_MIDDLEWARE_MSG);
                 assert.equal(res.body.globalRouterMidMsg, GLOBAL_ROUTER_MIDDLEWARE_MSG);
             });
+
+            await this.populateMocks();
+            await this.updateMocks();
             await this.assertModelMock1Routes();
             await this.assertModelMock2Routes();
             done();
@@ -88,17 +94,47 @@ class ProtonApplicationTest {
         }
     }
 
+    private async populateMocks(): Promise<any> {
+        return new Promise<ProtonApplication>((resolve, reject) => {
+            request(this.app.getExpress()).post('/mocks/modelmock1/').send({ id: 1, mockCol1: 'mockCol1', mockCol2: 1 }).expect(200).end((err, res) => {
+                request(this.app.getExpress()).post('/mocks/modelmock2/').send({ id: 1, mock2Col1: 'mock2Col1', mock2Col2: 2 } as Mock2).expect(200).end((err, res) => {
+                    request(this.app.getExpress()).post('/mocks/modelmock2/').send({ id: 1, mock2Col1: 'mock2Col1', mock2Col2: 2 } as Mock2).expect(200).end((err, res) => {
+                        resolve();
+                    });
+                });
+            });
+        });
+    }
+
+    private async updateMocks(): Promise<any> {
+        return new Promise<ProtonApplication>((resolve, reject) => {
+            request(this.app.getExpress()).put('/mocks/modelmock1/1').send({ mockCol1: 'mockCol1Updated' }).expect(200).end((err, res) => {
+                request(this.app.getExpress()).delete('/mocks/modelmock2/2').expect(200).end((err, res) => {
+                    resolve();
+                });
+            });
+        });
+    }
+
     private async assertModelMock1Routes() {
         await this.assertRouteGet("/mocks/modelmock1/", this.app.getExpress(), (err, res) => { assert.isNull(err); });
+        await this.assertRouteGet("/mocks/modelmock1/1", this.app.getExpress(), (err, res) => {
+            assert.isNull(err);
+            assert.equal(res.body.mockCol1, 'mockCol1Updated');
+        });
     }
 
     private async assertModelMock2Routes() {
         await this.assertRouteGet("/mocks/modelmock2/", this.app.getExpress(), (err, res) => { assert.isNull(err); });
+        await this.assertRouteGet("/mocks/modelmock2/1", this.app.getExpress(), (err, res) => {
+            assert.isNull(err);
+            assert.equal(res.body.mock2Col1, 'mock2Col1');
+        });
     }
 
-    private assertRouteGet(route: string, app: express.Application, assertFunction?: Function): Promise<any> {
+    private assertRouteGet(route: string, expressApp: express.Application, assertFunction?: Function): Promise<any> {
         return new Promise<ProtonApplication>((resolve, reject) => {
-            request(app).get(route)
+            request(expressApp).get(route)
                 .expect(200)
                 .end((err, res) => {
                     if (assertFunction) {
