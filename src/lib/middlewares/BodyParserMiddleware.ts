db613aaac512e482fc164a6ae7655e287a13c436
Humberto Machado <betolinck@gmail.com>
2018-03-10 15:25:15 -0300

    Reverse commit

--- src/lib/middlewares/BodyParserMiddleware.ts
+++ src/lib/middlewares/BodyParserMiddleware.ts
@@ -4,17 +4,13 @@ import { Middleware, MiddlewareFunctionParams } from "../decorators/MiddlewareCo
 export class BodyParserMiddleware extends ProtonMiddleware {
     @Middleware()
     public bodyParser(params: MiddlewareFunctionParams): void {
-        if (params.req.body == undefined || params.req.body == null) {
-            var data = '';
-            params.req.setEncoding('utf8');
-            params.req.on('data', (rawData) => data += rawData);
+        var data = '';
+        params.req.setEncoding('utf8');
+        params.req.on('data', (rawData) => data += rawData);
 
-            params.req.on('end', () => {
-                params.req.body = data;
-                params.next();
-            });
-        } else {
+        params.req.on('end', () => {
+            params.req.body = data;
             params.next();
-        }
+        });
     }
 }
