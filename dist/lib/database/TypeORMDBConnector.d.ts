8def3b8baad17f63d76a399974dbd02614ac59c8
Humberto Machado <betolinck@gmail.com>
2018-06-01 13:17:32 -0300

    Removed unecessary files

--- dist/lib/database/TypeORMDBConnector.d.ts
+++
@@ -1,8 +0,0 @@
-import { Connection, ConnectionOptions } from 'typeorm';
-import { DBConnector } from './DBConnector';
-export declare class TypeORMDBConnector implements DBConnector<ConnectionOptions, Connection> {
-    createConnection(options?: ConnectionOptions): Promise<Connection>;
-}
-export declare class TypeORMDB {
-    static getBD(): Connection;
-}
